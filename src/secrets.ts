838d839ad0d6e0e05606d4777b5d03afabe4c58f
feeleep <aliup45@gmail.com>
2024-05-30 17:24:43 +0100

    commiting database changes

--- src/secrets.ts
+++ src/secrets.ts
@@ -7,3 +7,4 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 
 export const JWT_SECRET = process.env.JWT_SECRET!;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN!;
+export const BASE_URL = process.env.BASE_URL;
