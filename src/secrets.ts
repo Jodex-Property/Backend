3bafcec7a5eaec5b414d80e7d992ce287e83961b
feeleep <aliup45@gmail.com>
2024-05-31 06:52:23 +0100

    commiting compiled javascript

--- src/secrets.ts
+++ src/secrets.ts
@@ -6,5 +6,5 @@ export const PORT = process.env.PORT || 4000;
 export const DATABASE_URL = process.env.DATABASE_URL;
 
 export const JWT_SECRET = process.env.JWT_SECRET!;
-export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN!;
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 export const BASE_URL = process.env.BASE_URL;
