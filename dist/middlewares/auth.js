7de5a4188f5a0722805d8e971f741e31fc4761bd
feeleep <aliup45@gmail.com>
2024-06-01 15:56:20 +0100

    database migration

--- dist/middlewares/auth.js
+++ dist/middlewares/auth.js
@@ -1,58 +1,45 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const unauthorized_1 = require("../exceptions/unauthorized");
-const root_1 = require("../exceptions/root");
-const jwt = __importStar(require("jsonwebtoken"));
-const secrets_1 = require("../secrets");
-const app_1 = require("../app");
-const authMiddleware = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const token = req.headers.authorization;
-    if (!token) {
-        next(new unauthorized_1.UnauthorizedException("You are not allowed to do this", root_1.ErrorCodes.UNAUTHORIZED));
-    }
-    try {
-        const payload = jwt.verify(token, secrets_1.JWT_SECRET);
-        const user = yield app_1.prisma.user.findFirst({ where: { id: payload.userId } });
-        if (!user) {
-            next(new unauthorized_1.UnauthorizedException("You are not allowed to do this", root_1.ErrorCodes.UNAUTHORIZED));
-        }
-        req.user = user;
-        next();
-    }
-    catch (error) {
-        next(new unauthorized_1.UnauthorizedException("You are not allowed to do this", root_1.ErrorCodes.UNAUTHORIZED));
-    }
-});
-exports.default = authMiddleware;
+// import { NextFunction, Request, Response } from "express";
+// import { BadRequests } from "../exceptions/badRequest";
+// import { UnauthorizedException } from "../exceptions/unauthorized";
+// import { ErrorCodes } from "../exceptions/root";
+// import * as jwt from "jsonwebtoken";
+// import { JWT_SECRET } from "../secrets";
+// import { prisma } from "../app";
+// const authMiddleware = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   const token = req.headers.authorization;
+//   if (!token) {
+//     next(
+//       new UnauthorizedException(
+//         "You are not allowed to do this",
+//         ErrorCodes.UNAUTHORIZED
+//       )
+//     );
+//   }
+//   try {
+//     const payload = jwt.verify(token, JWT_SECRET);
+//     const user = await prisma.user.findFirst({ where: { id: payload.userId } });
+//     if (!user) {
+//       next(
+//         new UnauthorizedException(
+//           "You are not allowed to do this",
+//           ErrorCodes.UNAUTHORIZED
+//         )
+//       );
+//     }
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     next(
+//       new UnauthorizedException(
+//         "You are not allowed to do this",
+//         ErrorCodes.UNAUTHORIZED
+//       )
+//     );
+//   }
+// };
+// export default authMiddleware;
